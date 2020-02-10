ab8498dbaf81122fd915572b8b8d2a5981dea04a
Andrew Steele <andrewsteele@ORRHS.ORG>
2020-02-10 13:56:01 -0500

    add event listeners

---
+++ Clicker Game/app.js
@@ -0,0 +1,4 @@
+let bananaImg;
+let heading;
+
+
--- Lesson 5.1/App.js
+++ Lesson 5.1/App.js
@@ -1,3 +1,49 @@
-function setup(){
-    
+let button;
+let greeting;
+
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     createElement('h1','my lucky numbers below');
+//     createElement('p','my lucky number is 7'); 
+// }
+
+// function draw() { 
+//     background(220);
+// } 
+
+// function setup() {
+//     createCanvas(windowWidth, windowHeight);
+//     let titleText = createElement('h1', 'My lucky number is...');
+//     let numText = createElement('p', '14');
+//     console.log(numText);
+// }
+
+// function mousePressed(){
+//     createP('My lucky number is' + random(0,100)); 
+// }
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    button = createButton('Try Me');
+    button.position(width / 2, height / 2)
+    button.mousePressed(greet)
+    greeting = createElement('h2', 'Say Hello!')
+    greeting.position(width / 2, height / 3)
+    textAlign(CENTER)
+    textSize(50)
+  }
+  
+
+function greet(){
+    greeting.html('Hello')
+
+    for (let i = 0; i < 200; i++){
+        push();
+        fill(random(255),255,255);
+        translate(random(width), random(height));
+        rotate(random(2 * PI));
+        text('HELLO',0,0);
+        pop();
+    }
 }
