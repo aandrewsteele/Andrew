7f0be8488eeba99db2d57e2454385ba558138037
Andrew Steele <andrewsteele@ORRHS.ORG>
2020-02-12 12:35:37 -0500

    nwewnewnew

---
+++ Lesson 3/Variables/app.js
@@ -0,0 +1,13 @@
+console.log;
+
+const add = 7 + 3;
+const sub = 7 - 3;
+const mult = 7 * 3;
+const div = 7 / 3;
+const paren = (83 + 92 + 86 + 94) / 4;
+
+let score = 0;
+console.log('Current Score: ' + score);
+
+let numLikes = 7;
+console.log('So far ' + numLikes + ' people like this page');
